219e9a45ef1eed7551b964daec158c701d22d69c
K42IE <katiepryal@live.com>
2024-03-02 09:57:12 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Book Recommender Agent
+
+This is a book recommender agent that helps users discover new books based on their preferences and interests.
+
+## Notes
+Sample Database: https://www.kaggle.com/datasets/arpansri/books-summary?resource=download
+
+
+
+
+## Contributing
+
+Contributions are welcome! If you have any ideas, suggestions, or bug reports, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](https://opensource.org/licenses/MIT).
+
---
+++ main.py
@@ -0,0 +1,42 @@
+from dotenv import load_dotenv
+import os
+import pandas as pd
+from llama_index.core.query_engine import PandasQueryEngine
+from prompts import new_prompt, instruction_str, context
+from note_engine import note_engine
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
+from llama_index.llms.openai import OpenAI
+load_dotenv()
+
+population_path = os.path.join("data", "books.csv")
+population_df = pd.read_csv(population_path, usecols=["book_name", "summary"])
+
+population_query_engine = PandasQueryEngine(
+    df=population_df, verbose=True, instruction_str=instruction_str
+)
+
+population_query_engine.update_prompts({"pandas_prompt": new_prompt})
+
+#population_query_engine.query("what is the Summary of the The Midnight Library")
+#population_query_engine.query("recomend me a book about a library")
+
+tools = [
+    note_engine,
+    QueryEngineTool(
+        query_engine=population_query_engine,
+        metadata=ToolMetadata(
+            name="population_data",
+            description="this gives information at the world population and demographics",
+        ),
+    ),
+]
+
+# This is the main agent
+llm = OpenAI(model="gpt-3.5-turbo-0613")
+agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, context=context)
+
+while (prompt := input("Enter a prompt (q to quit): ")) != "q":
+    result = agent.query(prompt)
+    print(result)
+
