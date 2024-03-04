72b83bc281f4febd168af4d6e1b83d280138dc88
K42IE <katiepryal@live.com>
2024-03-03 19:57:51 -0800

    clean up

--- prompts.py
+++ prompts.py
@@ -1,3 +1,2 @@
-from llama_index.core import PromptTemplate
 context = """Purpose: The primary role of this agent is to assist users by providing accurate 
             information/recomendations for books found in the dataset. """
